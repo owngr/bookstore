e9aabdb285fa812acd15f4e30ffb7567abc90dc2
Olivier Wenger <olivier.wenger@liip.ch>
2023-03-04 15:31:55 +0100

    added total for sale

--- src/frontend/src/views/Sales.vue
+++ src/frontend/src/views/Sales.vue
@@ -6,8 +6,9 @@
 
 
   </Dialog>
-  <div style="width: 15%">
-    <VueDatePicker v-model="date" range :preset-ranges="presetRanges" :enable-time-picker="false" :start-time="startTime" @update:model-value="handleDate">
+  <div style="width: 30%">
+    <VueDatePicker v-model="date" :enable-time-picker="false" :preset-ranges="presetRanges" :start-time="startTime"
+                   range @update:model-value="handleDate">
       <template #yearly="{ label, range, presetDateRange }">
         <span @click="presetDateRange(range)">{{ label }}</span>
       </template>
@@ -15,21 +16,38 @@
   </div>
 
   <DataTable
+      ref="dt"
       :value="invoices"
       responsive-layout="scroll"
-      striped-rows>
+      striped-rows
+  >
+    <template #header>
+      <div class="p-d-flex p-jc-between p-ai-center">
+        <button label="Export" @click="exportCSV($event)">Exporter</button>
+      </div>
+    </template>
     <column field="timeCreated" header="Date" :sortable="true"></column>
     <column field="id" header="ID"></column>
     <column field="quantity" header="Quantité"></column>
     <column field="paymentOption" header="Moyen de paiement" :sortable="true"></column>
     <column field="price" header="Montant"></column>
+    <ColumnGroup type="footer">
+      <Row v-for="[key, value] in total" :key="key">
+        <Column :colspan="4" :footer="key" style="font-weight: lighter"/>
+        <Column :colspan="1" :footer="value"/>
+      </Row>
+      <Row>
+        <Column :colspan="4" footer="Total"/>
+        <Column :colspan="1" :footer="totalSum"/>
+      </Row>
+    </ColumnGroup>
   </DataTable>
 </template>
 
 <script setup>
-import {ref} from "vue";
+import {computed, ref} from "vue";
 import {useFetchInvoices} from "@/composables/useFetch";
-import { endOfMonth, startOfMonth, startOfDay, endOfDay, startOfYear, endOfYear} from 'date-fns';
+import {endOfDay, endOfMonth, endOfYear, startOfDay, startOfMonth, startOfYear} from 'date-fns';
 import SaleService from "@/service/SaleService";
 
 const now = new Date()
@@ -43,18 +61,21 @@ const startTime = ref([
 );
 
 const presetRanges = ref([
-  { label: 'Aujourd\'hui', range: [startOfDay(now), endOfDay(now)] },
-  { label: 'Ce mois', range: [startOfMonth(now), endOfMonth(now)] },
-  { label: 'Cette année', range: [startOfYear(now), endOfYear(now)] },
+  {label: 'Aujourd\'hui', range: [startOfDay(now), endOfDay(now)]},
+  {label: 'Ce mois', range: [startOfMonth(now), endOfMonth(now)]},
+  {label: 'Cette année', range: [startOfYear(now), endOfYear(now)]},
 ]);
 
 
-const {invoices,messages} = useFetchInvoices(startDate, endDate)
+const {invoices, messages} = useFetchInvoices(startDate, endDate)
+
+// ref to the datatable
+const dt = ref()
 
 
 const handleDate = (modelData) => {
   date.value = modelData;
-  SaleService.getSales(date.value[0],date.value[1])
+  SaleService.getSales(date.value[0], date.value[1])
       .then((response) => response.json())
       .then((data) => {
         invoices.value = data
@@ -64,7 +85,30 @@ const handleDate = (modelData) => {
       })
 }
 
+const exportCSV = () => {
+  dt.value.exportCSV();
+}
 
+const total = computed(() => {
+  const accumulator = new Map()
+  accumulator.set('CASH', 0)
+  accumulator.set('CARD', 0)
+  accumulator.set('TWINT', 0)
+  accumulator.set('VOUCHER', 0)
+  invoices.value.forEach(invoice => {
+    accumulator.set(invoice.paymentOption, Math.round((accumulator.get(invoice.paymentOption) + invoice.price) * 100) / 100)
+  })
+  console.debug(accumulator)
+  return accumulator
+})
+
+const totalSum = computed(() => {
+  let sum = 0
+  total.value.forEach(value => {
+    sum = Math.round((sum + value) * 100) / 100
+  })
+  return sum
+})
 
 let displayViewDialog = ref(false)
 
